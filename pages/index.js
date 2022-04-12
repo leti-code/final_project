45c0d2579a7cc5f7860898b5def14f1b12536d96
leti-code <leticiacasabella@gmail.com>
2022-04-12 14:21:38 +0200

    fix(vercel): testing vercel problems

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,29 @@
+import React from 'react';
 import MainLayout from "layouts/MainLayout"
 
-const Index = ({ }) => (
-  <MainLayout>
-  INDEX
-  </MainLayout>
-)
+const Index = () => {
+  const prueba = async (e) => {
+    console.log("click....");
+    e.preventDefault();
+    const res = await fetch('/api/user/', {
+      method: 'GET',
+        headers: {
+          'Content-Type': 'application/json',
+          },
+      })
 
-export default Index
+      const json = await res.json();
+
+      console.log(json);
+  };
+
+  return(
+    <MainLayout>
+    INDEX
+    <button onClick={(e)=>prueba(e)}>prueba</button>
+    </MainLayout>
+  );
+} 
+
+export default Index;
+
