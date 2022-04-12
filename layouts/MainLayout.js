680a79a52f8f89d699c1192422d2b148229179e9
leti-code <leticiacasabella@gmail.com>
2022-04-12 12:40:58 +0200

    feat(layout): added mainLAyout

--- layouts/MainLayout.js
+++ layouts/MainLayout.js
@@ -1,11 +1,14 @@
-import React from 'react'
+import React from 'react';
+import Header from '../components/Header';
+import Footer from '../components/Footer';
+
 
 const MainLayout = ({children}) => {
   return (
     <div>
-        <div>HEADER</div>
+        <Header/>
         {children}
-        <div>FOOTER</div>
+        <Footer/>
     </div>
   )
 }
