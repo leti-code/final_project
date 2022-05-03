bf76d45eb294cae51317a9d2631f59b696c4f33e
leti-code <leticiacasabella@gmail.com>
2022-05-03 19:26:33 +0200

    feat(all): add variables to reducer

--- store/slices/user/user.slice.js
+++ store/slices/user/user.slice.js
@@ -3,7 +3,13 @@ import { createSlice } from "@reduxjs/toolkit";
 const initialState= {
     logged: false,
     token: null,
-    username: null        
+    username: null  ,
+    email : null,
+    img : null,
+    active_maps : null,
+    actual_flag : null,
+    scores : null,
+    maps_owned : null      
 }
 
 export const userSlice = createSlice({
@@ -14,6 +20,12 @@ export const userSlice = createSlice({
             state.logged = action.payload.logged;
             state.token = action.payload.token;
             state.username = action.payload.username;
+            state.email = action.payload.email;
+            state.img = action.payload.img;
+            state.active_maps = action.payload.active_maps;
+            state.actual_flag = action.payload.actual_flag;
+            state.scores = action.payload.scores;
+            state.maps_owned = action.payload.maps_owned;
             window.localStorage.setItem("byb_token", action.payload.token);
         }
     }
