d4311284498cf40d91dc2e39f83f6436811988a3
leti-code <75835841+leti-code@users.noreply.github.com>
2024-07-03 10:08:20 +0200

    Update eslint.config.js

--- eslint.config.js
+++ eslint.config.js
@@ -1,8 +1,12 @@
-module.exports = [{
-    rules: {
-      "indent": ["error", 2],
-      "linebreak-style": ["error", "unix"],
-      "quotes": ["error", "double"],
-      "semi": ["error", "always"]
+module.exports = [
+    //podemos tener distintos objetos con configs distintas
+    {
+        ignores: ["**/eslint.config.js"], //ignora el archivo de configuración del linting a la hora de evaluarlo
+        files: ["**/*.ts"], //por defecto analiza files con extensión .js .cjs y .mjs, así incluimos que analice los archivos typescript
+        rules: { 
+          "indent": ["error", 2],
+          "linebreak-style": ["error", "unix"],
+          "quotes": ["warn", "double"],
+          "semi": ["error", "always"] //especifica que la regla del ; tiene severidad de error
     }
 }];
