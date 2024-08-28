e0ba9a27ed3545cd9407acbdac655b7d266fd65b
Leticia Casabella <leticia.casabella@external.wolterskluwer.com>
2024-08-28 13:44:24 +0200

    files compressed

--- next.config.js
+++ next.config.js
@@ -2,6 +2,7 @@ const withPWA = require("next-pwa");
 
 //Configuration of pwa and allowed domains to display images (apart from local)
 module.exports = withPWA({
+    output:"standalone",
     pwa: {
         dest: "public"
     },
