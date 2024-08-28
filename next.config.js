225379febe7cab41212032bd1d216f51cba360f6
Leticia Casabella <leticia.casabella@external.wolterskluwer.com>
2024-08-28 13:59:19 +0200

    delete compression

--- next.config.js
+++ next.config.js
@@ -1,15 +1,13 @@
 const withPWA = require("next-pwa");
 
 //Configuration of pwa and allowed domains to display images (apart from local)
-module.exports = 
-// withPWA({
-    {output:"standalone"};
-//     pwa: {
-//         dest: "public"
-//     },
-//     images: {
-//         domains: [
-//             "res.cloudinary.com"
-//         ],
-//     }
-// });
+module.exports = withPWA({
+    pwa: {
+        dest: "public"
+    },
+    images: {
+        domains: [
+            "res.cloudinary.com"
+        ],
+    }
+});
